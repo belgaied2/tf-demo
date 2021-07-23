ba6e171543dbb440dfc820c4bc999f78e9d743a9
Mohamed Belgaied Hassine <60746935+belgaied2@users.noreply.github.com>
2021-07-23 09:57:01 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  version = "2.41"
+  # aws_access_key = 
+  # aws_secret_key = 
+  region = "eu-central-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name      = "MacOSsKey"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
